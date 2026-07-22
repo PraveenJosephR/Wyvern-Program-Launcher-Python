d1d3d981c9a9193d73270bc1c7f983bf82a3cb9a
PraveenJosephR <pj.ratniah@gmail.com>
2026-07-22 18:12:45 +0530

    added icon and build command in main file

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
-
+from PySide6.QtGui import QIcon
 
 def main():
     app = QApplication(sys.argv)
-
+    app.setWindowIcon(QIcon("resources/app.ico"))
     window = MainWindow()
+    window.setWindowIcon(QIcon("resources/app.ico"))
     window.show()
 
     sys.exit(app.exec())
@@ -15,3 +16,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    # build call ----> pyinstaller --onefile --windowed --icon=resources/app.ico main.py
